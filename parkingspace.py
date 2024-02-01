aed4ee2be712ece3ebe50fe70f4ee375b5edb579
Christian Hey <c.hey@sbs-herzogenaurach.de>
2024-02-01 13:16:55 +0100

    Solved task 1 Added task 2

---
+++ parkingspace.py
@@ -0,0 +1,5 @@
+# Erstellen Sie eine mehrdimensionale Liste für ein Parkhaus (car_park).
+# Das Parkhaus besteht aus:
+# - drei Ebenen (level)
+# - pro Ebene fünf Reihen (row)
+# - jede Reihe hat 20 Parkplätze (lot)
