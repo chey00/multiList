9e86e30ebae01235c0cb47bb5277c79d0adfe5d2
Christian Hey <c.hey@sbs-herzogenaurach.de>
2024-02-01 13:22:56 +0100

    Added task 3

--- parkingspace.py
+++ parkingspace.py
@@ -3,3 +3,13 @@
 # - drei Ebenen (level)
 # - pro Ebene fünf Reihen (row)
 # - jede Reihe hat 20 Parkplätze (lot)
+
+# Aufgabe 2
+# Berechnen Sie die Anzahl an Parkplätzen des Parkhauses zur Laufzeit.
+
+# Aufgabe 3
+# Erstellen Sie für jeden Parkplatz ein Wörterbuch, welches folgende
+# Schlüsselwörter hat:
+# - Ankunftszeit (arrival)
+# - Abfahrtszeit (departure)
+# - Kennzeichen (license plate)
